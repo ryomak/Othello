636ceb430c5d33837dc4ebbac22b1260efd60851
RyomaK <ryooomaaa0413@outlook.com>
2017-12-15 02:02:10 +0900

    add

---
+++ define.h
@@ -0,0 +1,37 @@
+#ifndef _DEFINE_H_
+#define _DEFINE_H_
+
+#define LEN 8
+
+void Init();
+void Display();
+void DecideTurn();
+int ctoi(char);
+void Locate(char*,int*,int*);
+void PlayerMove();
+void COMMove();
+void COM1Move();
+int CheckFlip(int,int,int,int);
+int Check(int,int,int);
+void Flip(int,int,int,int);
+int Put(int,int,int);
+void ShowAnswer(int,int);
+int Judge(int);
+void Counter();
+int MinMax(int[LEN][LEN],int,int);
+int Evaluate(int[LEN][LEN]);
+
+
+int Board[8][8] = {
+	{0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0},
+};
+
+
+#endif
--- main.c
+++ main.c
@@ -4,34 +4,8 @@ MoveのCOM1MoveをPlayerMoveにすると変更できる
 
 #include <stdio.h>
 #include <string.h>
-#define LEN 8
-
-void Init();
-void Display();
-void DecideTurn();
-int ctoi(char);
-void Locate(char*,int*,int*);
-void PlayerMove();
-void COMMove();
-int CheckFlip(int,int,int,int);
-int Check(int,int,int);
-void Flip(int,int,int,int);
-int Put(int,int,int);
-void ShowAnswer(int,int);
-int Judge(int);
-void Counter();
-
-
-int Board[8][8] = {
-	{0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0},
-};
+#include "define.h"
+#include "evaluate.h"
 
 //初期data
 //player の順番
@@ -48,9 +22,87 @@ int COM ;
 //方向 左上から時計回り
 int vec_x[]={-1,0,1,1,1,0,-1,-1};
 int vec_y[]={1,1,1,0,-1,-1,-1,0};
+//minmax search
+int SEARCH_LEVEL = 6;
 
+//==============main=====================-=====
+int main(){
+	Init();
+	while(available){
+		switch(Judge(turn)){
+		case 0: Move();break;
+		case 1: turn = turn*-1; Move();break;
+		case 2: Counter(); break;
+		default: available = 0;break;
+		}
+	}
+	return 0;
+}
+//============ゲームの仕組み=======================
+void Move(){
+	if(offence == 1){
+		if(turn == 1){
+			//PlayerMove();
+			COM1Move();
+		}else{
+			COMMove();
+		}
+	}else{
+		if(turn == 1){
+			COMMove();
+		}else{
+			//PlayerMove();
+			COM1Move();
+		}
+	}
+}
 
+//0...自分の手はある
+//1...相手は出せる
+//2...どちらも出せない
+int Judge(int turn){
+	int x,y;
+	for(x=0;x<LEN;x++){
+		for(y=0;y<LEN;y++){
+			if(Check(turn,x,y) == 1) return 0;
+		}
+	}
+	//パスしてターン変更した場合
+	for(x=0;x<LEN;x++){
+		for(y=0;y<LEN;y++){
+			if(Check(turn*-1,x,y) == 1) return 1;
+		}
+	}
+	return 2;
+}
 
+void Counter(){
+	printf("~~~~\n");
+	int x,y;
+	int o_point = 0;
+	int x_point = 0;
+	for(x=0;x<LEN;x++){
+		for(y=0;y<LEN;y++){
+			switch(Board[y][x]){
+				case 1: x_point++; break;
+				case -1: o_point++; break;
+				default: break;
+			}
+		}
+	}
+	printf("o:%d\n",o_point);
+	printf("x:%d\n",x_point);
+	if(x_point>o_point){
+		printf("winner: x\n");
+	}else if(x_point == o_point){
+		printf("draw\n");
+	}else{
+		printf("winner: o\n");
+	}
+	available = 0;
+}
+
+//==================表示=======================
 void Display(){
 	int y,x;
 	printf(" abcdefgh\n");
@@ -75,15 +127,29 @@ void Display(){
 	}
 }
 
+
+//===========初期化============================
+void Init(){
+	DecideTurn();
+	available = 1;
+	//o
+	Board[3][3] = -1;
+	Board[4][4] = -1;
+	//x
+	Board[4][3] = 1;
+	Board[3][4] = 1;
+	Display();
+}
+
 void DecideTurn(){
 	char c;
-	printf("先行(x)か後攻(o)か選んでください ");
+	printf("先行(x)か後攻(o)か選んでください \n");
 	while(1){
 		scanf("%c",&c);
 		if ((c == 'x') || (c == 'o')){
 			break;
 		}else{
-			printf("先行(x)か後攻(o)か選んでください ");
+			printf("先行(x)か後攻(o)か選んでください \n");
 
 		}
 	}
@@ -98,19 +164,7 @@ void DecideTurn(){
 	}
 }
 
-void Init(){
-	DecideTurn();
-	available = 1;
-	//o
-	Board[3][3] = -1;
-	Board[4][4] = -1;
-	//x
-	Board[4][3] = 1;
-	Board[3][4] = 1;
-	Display();
-}
-
-//a=0,b=1
+//=================ヘルパー===============
 int ctoi(char c) {
 	if (c >= '1' && c <= '9') {
 		return c - '1';
@@ -132,6 +186,23 @@ void Locate(char* str,int* x,int* y){
 		default: *x = -1; break;
 	}
 }
+
+void ShowAnswer(int x,int y){
+	printf("%d,",y+1);
+	switch(x){
+		case 0:printf("a"); break;
+		case 1:printf("b"); break;
+		case 2:printf("c"); break;
+		case 3:printf("d"); break;
+		case 4:printf("e"); break;
+		case 5:printf("f"); break;
+		case 6:printf("g"); break;
+		case 7:printf("h"); break;
+	}
+	printf("\n");
+}
+
+//===============ひっくり返す仕組み===============
 int Check(int turn,int x,int y){
 	int vec;
 	if(Board[y][x] == 0){
@@ -182,9 +253,7 @@ void Flip(int turn,int x,int y,int vec){
 		Board[y][x] = turn;
 	}
 }
-
-
-
+//=========playe===========r============
 void PlayerMove(){
 	while(1){
 		printf("石を置く場所を指定してください ");
@@ -203,32 +272,19 @@ void PlayerMove(){
 	Display();
 }
 
-void ShowAnswer(int x,int y){
-	printf("%d,",y+1);
-	switch(x){
-		case 0:printf("a"); break;
-		case 1:printf("b"); break;
-		case 2:printf("c"); break;
-		case 3:printf("d"); break;
-		case 4:printf("e"); break;
-		case 5:printf("f"); break;
-		case 6:printf("g"); break;
-		case 7:printf("h"); break;
-	}
-	printf("\n");
-}
-
+//======COMPUTER===========================
+//最弱
 void COM1Move(){
 	printf("コンピュータ1の手 ");
 	int ans_x;
 	int ans_y;
 	int x,y;
+	int i =0;
 	for(x=0;x<8;x++){
 		for(y=0;y<8;y++){
 			if(Check(Player,x,y)==1){
-				ans_x = x;
-				ans_y = y;
-				break;
+					ans_x = x;
+					ans_y = y;
 			}
 		}
 	}
@@ -237,100 +293,116 @@ void COM1Move(){
 	turn = turn*-1;
 	Display();
 }
-
+//==============ミニマックス法========================
 void COMMove(){
 	printf("コンピュータの手 ");
 	int ans_x;
 	int ans_y;
-	int x,y;
-		for(x=0;x<8;x++){
-			for(y=0;y<8;y++){
-				if(Check(COM,x,y)==1){
-					ans_x = x;
-					ans_y = y;
-					break;
-				}
-			}
-
-		}
-	Put(COM,ans_x,ans_y);
+	int val = MinMax(Board,COM,SEARCH_LEVEL);
+	ans_x = val%LEN;
+	ans_y = val/LEN;
 	ShowAnswer(ans_x,ans_y);
+	Put(COM,ans_x,ans_y);	
 	turn = turn*-1;
 	Display();
 }
-//0...自分の手はある
-//1...相手は出せる
-//2...どちらも出せない
-int Judge(int turn){
-	int x,y;
-	for(x=0;x<LEN;x++){
-		for(y=0;y<LEN;y++){
-			if(Check(turn,x,y) == 1) return 0;
-		}
-	}
-	//パスしてターン変更した場合
-	for(x=0;x<LEN;x++){
-		for(y=0;y<LEN;y++){
-			if(Check(turn*-1,x,y) == 1) return 1;
-		}
-	}
-	return 2;
-}
 
-void Counter(){
-	printf("~~~~\n");
+int MinMax(int board[8][8],int flag,int level){
+	// ノードの評価値
+	int value;
+    // 子ノードから伝播してきた評価値
+    int childValue;
+    // ミニマックス法で求めた最大の評価値を持つ場所
+    int bestX = -1;
+    int bestY = -1;
+	
+
+	//copy
+	int undo_board[8][8];
+	int i;
+
+	if(level==0){
+		return Evaluate(board);
+	}
+	//
+	if(flag==Player){
+		value = 1000;
+	}else{
+		value = -1000;
+	}
+
+	int judge = Judge(flag);
 	int x,y;
-	int o_point = 0;
-	int x_point = 0;
-	for(x=0;x<LEN;x++){
-		for(y=0;y<LEN;y++){
-			switch(Board[y][x]){
-				case 1: x_point++; break;
-				case -1: o_point++; break;
-				default: break;
+	if(judge==0){
+		for(x=0;x<LEN;x++){
+			for(y=0;y<LEN;y++){
+				if(Check(flag,x,y)==1){
+					for(i=0;i<LEN;i++) memcpy( undo_board[i], board[i], sizeof(int)*LEN );
+					Put(flag,x,y);
+					childValue = MinMax(board,flag*-1, level - 1);
+					if(flag==Player){
+						if (childValue < value) {
+							value = childValue;
+							bestX = x;
+							bestY = y;
+						}
+					}else{
+						if (childValue > value) {
+							value = childValue;
+							bestX = x;
+							bestY = y;
+						}	
+					}
+					for(i=0;i<LEN;i++) memcpy( board[i], undo_board[i], sizeof(int)*LEN );
+				}
 			}
 		}
-	}
-
-	printf("o:%d\n",o_point);
-	printf("x:%d\n",x_point);
-	if(x_point>o_point){
-		printf("winner: x\n");
-	}else if(x_point == o_point){
-		printf("draw\n");
+	}else if(judge==1){
+		return value;
 	}else{
-		printf("winner: o\n");
+		return value;
 	}
-	available = 0;
+	turn = flag;
+	if (level == SEARCH_LEVEL) {
+	// ルートノードなら最大評価値を持つ場所を返す
+	//printf("user:%d (x,y)=(%d,%d)\n",flag,bestX,bestY);
+	printf("eva:%d\n",value);
+		return bestX+bestY*LEN;
+	} else {
+		// 子ノードならノードの評価値を返す
+		return value;
+	}
+	
 }
-
-void Move(){
-	if(offence == 1){
-		if(turn == 1){
-			PlayerMove();
-			//COM1Move();
-		}else{
-			COMMove();
+int Evaluate(int board[LEN][LEN]){
+	int x,y;
+	int eva=0;
+	for(x=0;x<LEN;x++){
+		for(y=0;y<LEN;y++){
+			eva += board[y][x]*eva_board[y][x]; 
 		}
+	}	
+	if(COM == 1){
+		return eva;
 	}else{
-		if(turn == 1){
-			COMMove();
-		}else{
-			PlayerMove();
-			//COM1Move();
-		}
+		return -eva;
 	}
 }
 
-int main(){
-	Init();
-	while(available){
-		switch(Judge(turn)){
-		case 0: Move();break;
-		case 1: turn = turn*-1; Move();break;
-		case 2: Counter(); break;
-		default: available = 0;break;
+/*
+//こっちは駒の個数をどれだけ多く取るかを計算する
+int Evaluate(int board[LEN][LEN]){
+	int x,y;
+	int eva=0;
+	for(x=0;x<LEN;x++){
+		for(y=0;y<LEN;y++){
+			eva += board[y][x]; 
 		}
+	}	
+	if(COM == 1){
+		return eva;
+	}else{
+		return -eva;
 	}
-	return 0;
 }
+*/
